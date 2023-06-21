135051eca2fe5cad1def2bf05e9e70e2669cfdb2
LaxmiPriyaP <81460800+LaxmiPriyaP@users.noreply.github.com>
2023-06-21 14:47:56 +0530

    Add files via upload

---
+++ LogicalNOT.java
@@ -0,0 +1,19 @@
+package Basics;
+
+public class LogicalNOT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10, b = 1;
+		 
+        // Displaying a, b, c
+        System.out.println("Var1 = " + a);
+        System.out.println("Var2 = " + b);
+ 
+        // Using logical NOT operator
+        System.out.println("!(a < b) = " + !(a < b));
+        System.out.println("!(a > b) = " + !(a > b));
+
+	}
+
+}
