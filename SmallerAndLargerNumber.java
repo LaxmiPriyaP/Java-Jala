b2b52b3e9bf2794e50874c25767994194b11d816
LaxmiPriyaP <81460800+LaxmiPriyaP@users.noreply.github.com>
2023-06-21 15:13:12 +0530

    Add files via upload

---
+++ SmallerAndLargerNumber.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class SmallerAndLargerNumber {
+
+	public static void main(String[] args) {
+		int num1 = 100;
+		int num2 = 111;
+ 
+		System.out.println("The largest number of the two numbers is " + Math.max(num1,num2));
+		System.out.println("The smallest number of the two numbers is " + Math.min(num1,num2));
+
+	}
+
+}
