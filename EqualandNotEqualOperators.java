dac793f15aefb23a6ddbd6626277549d3702adc3
LaxmiPriyaP <81460800+LaxmiPriyaP@users.noreply.github.com>
2023-06-21 14:22:55 +0530

    Add files via upload

---
+++ EqualandNotEqualOperators.java
@@ -0,0 +1,16 @@
+package Basics;
+
+public class EqualandNotEqualOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int a,b;
+	a= 2;
+	b= 10;
+	if (a==b) {
+	System.out.println("a is equal to b");}
+	else if(a!=b){
+		System.out.println("a is not equal to be");
+	}
+	}
+}
